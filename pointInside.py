50b6c659b2a48eb2d1cf2832c9fce929b5841bf6
sasamil <sasa.milenkovic.xyz@gmail.com>
2016-04-29 16:18:46 +0200

    Added files via upload
    pointsInside.py - statndard (well known) implementations and my own implementation in is_inside_sm() function. Unlike standard implementations, is_inside_sm() WORKS ON EDGES. Here, we also have is_inside_postgis() function. It is implementation from PostGIS library, translated from C to Python (with numerous asserts removed, for the performance testing). This implementaton DOES work on edges - it is a must on GIS systems.
    
    After all - it turns out that is_inside_sm() is the fastest and in addition, it works on edges.
    
    test.py - Test application for testing performancies.

---
+++ pointInside.py
@@ -0,0 +1,184 @@
+# the representation of a point will be a tuple (x,y)
+# the representation of a polygon wil be a list of points [(x1,y1), (x2,y2), (x3,y3), ... ]
+
+# it is assumed that polygon is regular i.e. lines don't intersect each other (otherwise, it is questionable whether it is a polygon)
+
+#-------------------------------------------------------------------------------
+# isLeft(): tests if a point is Left|On|Right of an infinite line.
+#    Input:  three points P0, P1, and P2
+#    Return: >0 for P2 left of the line through P0 and P1
+#            =0 for P2  on the line
+#            <0 for P2  right of the line
+#    See: Algorithm 1 "Area of Triangles and Polygons"
+def isLeft(P0, P1, P2):
+  return ((P1[0] - P0[0]) * (P2[1] - P0[1]) - (P2[0] - P0[0]) * (P1[1] - P0[1]))
+
+
+#-------------------------------------------------------------------------------
+# wn_PnPoly(): winding number test for a point in a polygon
+#      Input:   P = a point,
+#               V[] = vertex points of a polygon V[n+1] with V[n]=V[0]
+#      Return:  wn = the winding number (=0 only when P is outside)
+def wn_PnPoly(P, V):
+  # in their approach the list is one member longer. The last and first points coincide.
+  #V = copy.deepcopy(V)
+  #V.append(V[0]) 
+
+  #n = len(V0)
+
+  n = len(V)-1
+  wn = 0;    # the  winding number counter
+  i = 0
+
+  # loop through all edges of the polygon
+  while i<n:   # edge from V[i] to  V[i+1]
+    if V[i][1] <= P[1]:         # start y <= P.y
+      if V[i+1][1]  > P[1]:      # an upward crossing
+        if isLeft(V[i], V[i+1], P) > 0:  # P left of  edge
+          wn += 1            # have  a valid up intersect
+    else:                        # start y > P.y (no test needed)
+      if V[i+1][1] <= P[1]:     # a downward crossing
+        if isLeft(V[i], V[i+1], P) < 0:  # P right of  edge
+          wn -= 1            # have  a valid down intersect
+
+    i += 1
+  
+  #print str(wn)
+  return wn;
+
+#-------------------------------------------------------------------------------
+# cn_PnPoly(): crossing number test for a point in a polygon
+#      Input:   P = a point,
+#               V[] = vertex points of a polygon V[n+1] with V[n]=V[0]
+#      Return:  0 = outside, 1 = inside
+# This code is patterned after [Franklin, 2000]
+# Taken from: http://geomalgorithms.com/a03-_inclusion.html
+
+def cn_PnPoly(P, V): # P - Point; V - Polygon
+
+  # in their approach the list is one member longer. The last and first points coincide.
+  #V = copy.deepcopy(V0)
+  #V.append(V[0]) 
+
+  #n = len(V0)
+
+  n = len(V)-1
+  cn = 0    # the  crossing number counter
+
+  # loop through all edges of the polygon
+  i = 0
+  # while i<n: # edge from V[i]  to V[i+1]
+  for i in range(n):
+    # upward crossing or downward crossing
+    if (V[i][1] <= P[1] and V[i+1][1] > P[1]) or (V[i][1] > P[1] and V[i+1][1] <= P[1]): 
+      # compute  the actual edge-ray intersect x-coordinate
+      vt = (P[1]  - V[i][1]) / (V[i+1][1] - V[i][1])
+      # if P[0] > (V[i][0] + vt * (V[i+1][0] - V[i][0])): # P.x > intersect - ray toward left
+      if P[0] < (V[i][0] + vt * (V[i+1][0] - V[i][0])): # P.x < intersect - ray toward right - original
+        cn += 1   # a valid crossing of y=P[1] right of P.x
+
+    i += 1
+    
+  #print str(cn)
+  return (cn&1)    # 0 if even (out), and 1 if  odd (in)
+
+#-------------------------------------------------------------------------------
+# x, y -- x and y coordinates of point
+# poly -- a list of tuples [(x, y), (x, y), ...]
+def isPointInPath(P, poly):
+        num = len(poly)
+        i = 0
+        j = num - 1
+        c = False
+        for i in range(num):
+                if  ((poly[i][1] > P[1]) != (poly[j][1] > P[1])) and \
+                        (P[0] < (poly[j][0] - poly[i][0]) * (P[1] - poly[i][1]) / (poly[j][1] - poly[i][1]) + poly[i][0]):
+                    c = not c
+                j = i
+
+        return c
+
+#-------------------------------------------------------------------------------
+# This function gives the answer whether the given point is inside or outside the predefined polygon
+# Unlike standard ray-casting algorithm, this one works on edges! (with no performance cost)
+# According to performance tests - this is the best variant.
+
+# arguments:
+# Polygon - searched polygon
+# Point - an arbitrary point that can be inside or outside the polygon
+# length - the number of point in polygon (Attention! The list itself has an additional member - the last point coincides with the first)
+
+# return value:
+# 0 - the point is outside the polygon
+# 1 - the point is inside the polygon 
+# 2 - the point is one edge (boundary)
+
+def is_inside_sm(polygon, point):
+  length = len(polygon)-1
+  dy2 = point[1] - polygon[0][1]
+  intersections = 0
+  ii = 0
+  jj = 1
+
+  while ii<length:
+    dy  = dy2
+    dy2 = point[1] - polygon[jj][1]
+
+    # consider only lines which are not completely above/bellow/right from the point
+    if dy*dy2 <= 0.0 and (point[0] >= polygon[ii][0] or point[0] >= polygon[jj][0]):
+        
+      # non-horizontal line
+      if dy<0 or dy2<0:
+        F = dy*(polygon[jj][0] - polygon[ii][0])/(dy-dy2) + polygon[ii][0]
+
+        if point[0] > F: # if line is left from the point - the ray moving towards left, will intersect it
+          intersections += 1
+        elif point[0] == F: # point on line
+          return 2
+
+      # point on upper peak (dy2=dx2=0) or horizontal line (dy=dy2=0 and dx*dx2<=0)
+      elif dy2==0 and (point[0]==polygon[jj][0] or (dy==0 and (point[0]-polygon[ii][0])*(point[0]-polygon[jj][0])<=0)):
+        return 2
+
+      # there is another posibility: (dy=0 and dy2>0) or (dy>0 and dy2=0). It is skipped 
+      # deliberately to prevent break-points intersections to be counted twice.
+    
+    ii += 1
+    jj += 1
+            
+  #print 'intersections =', intersections
+  return intersections & 1  
+
+#-------------------------------------------------------------------------------
+def is_inside_postgis(polygon, point):
+  length = len(polygon)
+  intersections = 0
+
+  dx2 = point[0] - polygon[0][0]
+  dy2 = point[1] - polygon[0][1]
+  ii = 0
+  jj = 1
+
+  while jj<length:
+    dx  = dx2
+    dy  = dy2
+    dx2 = point[0] - polygon[jj][0]
+    dy2 = point[1] - polygon[jj][1]
+
+    F =(dx-dx2)*dy - dx*(dy-dy2);
+    if 0.0==F and dx*dx2<=0 and dy*dy2<=0:
+      return 2;
+
+    if (dy>=0 and dy2<0) or (dy2>=0 and dy<0):
+      if F > 0:
+        intersections += 1
+      elif F < 0:
+        intersections -= 1
+    
+    ii += 1
+    jj += 1
+            
+  #print 'intersections =', intersections
+  return intersections != 0  
+
+
---
+++ test.py
@@ -0,0 +1,78 @@
+# the representation of a point will be a tuple (x,y)
+# the representation of a polygon wil be a list of points [(x1,y1), (x2,y2), (x3,y3), ... ]
+
+# it is assumed that polygon is regular i.e. lines don't intersect each other (otherwise, it is questionable whether it is a polygon)
+
+import sys
+import copy
+import time
+import random
+
+from pointInside import *
+
+#-------------------------------------------------------------------------------
+test_polygon2 = [ (0,1), (1,1), (1,2), (2,2), (2,3), (3,3), (3,2), (4,0), (5,9), (6,0), 
+              (7,2), (8,0), (8,-2), (7,-3), (6,-2), (5,-2), (4,-2), (3,-1), (2,-2), (1,-2), 
+              (0,-3), (-2,-3), (-3,-4), (-4,-3), (-5,-3), (-5,-2), (-4,-2), (-4,.5), (-5,.5), (-5,1), 
+              (-4,1), (-4,2), (-4,4), (-3,4), (-3,2), (-2,2), (-2,0), (-1,1), (0,1) ]
+
+
+
+#-------------------------------------------------------------------------------
+def main():
+  
+  random.seed(1389)
+  i = 0
+  t = time.clock()
+  while i < 50000:
+    testpoint = (0.1*random.randrange(-50, 80), 0.1*random.randrange(-40, 40))
+    cn_PnPoly(testpoint, test_polygon2)
+    i += 1
+  
+  print "cn_PnPoly() - execution time: " + str(time.clock() - t)
+
+  random.seed(1389)
+  i = 0
+  t = time.clock()
+  while i < 50000:
+    testpoint = (0.1*random.randrange(-50, 80), 0.1*random.randrange(-40, 40))
+    wn_PnPoly(testpoint, test_polygon2)
+    i += 1
+  
+  print "wn_PnPoly() - execution time: " + str(time.clock() - t)
+
+  random.seed(1389)
+  i = 0
+  t = time.clock()
+  while i < 50000:
+    testpoint = (0.1*random.randrange(-50, 80), 0.1*random.randrange(-40, 40))
+    is_inside_postgis(test_polygon2, testpoint)
+    i += 1
+  
+  print "is_inside_postgis() - execution time: " + str(time.clock() - t)
+
+  random.seed(1389)
+  i = 0
+  t = time.clock()
+  while i < 50000:
+    testpoint = (0.1*random.randrange(-50, 80), 0.1*random.randrange(-40, 40))
+    isPointInPath(testpoint, test_polygon2)
+    i += 1
+  
+  print "isPointInPath() - execution time: " + str(time.clock() - t)
+
+  random.seed(1389)
+  i = 0
+  t = time.clock()
+  while i < 50000:
+    testpoint = (0.1*random.randrange(-50, 80), 0.1*random.randrange(-40, 40))
+    is_inside_sm(test_polygon2, testpoint)
+    i += 1
+  
+  print "is_inside_sm() - execution time: " + str(time.clock() - t)
+
+  
+
+# -------------------------------------------------------------------------------
+if __name__ == '__main__':
+  main()
